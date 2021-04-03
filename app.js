bd6fc551c7692dcba1f1d78e2a95027a075b6dbd
Brenno <brennoogeda@gmail.com>
2021-04-03 16:30:28 -0300

    menu com javascript

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ window.onload = () => {
   //Carregar estrutura do menu
   iniciaEfeitoMenu();
 };
+
+function iniciaEfeitoMenu() {
+  let menu = document.querySelector(".menu");
+
+  window.onscroll = manipulaMenu;
+
+  function manipulaMenu() {
+    let posicaoScroll = window.pageYOffset;
+
+    if (posicaoScroll > 60) {
+      menu.classList.add("menu-escuro");
+    } else {
+      menu.classList.remove("menu-escuro");
+    }
+
+    menu.style.backgroundColor =
+      "rgba(255,255,255, " + posicaoScroll / 100 + ")";
+  }
+}
